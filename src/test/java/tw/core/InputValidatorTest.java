49a4d3b5c6c9985784a2ae6ec91dacf5df0c441d
FeiXie-Liam <xiefei0887@163.com>
2018-04-20 15:21:05 +0800

    finish all

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,32 @@
 package tw.core;
 
+import org.junit.Test;
+import tw.validator.InputValidator;
+
+import static org.junit.Assert.assertFalse;
+
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
+    private InputValidator inputValidator;
 
+    public InputValidatorTest() {
+        inputValidator = new InputValidator();
+    }
+
+    @Test
+    public void should_returnFalse_when_inputSizeIsNot4() {
+        String input = "1 2 3 5 8";
+        assertFalse(inputValidator.validate(input));
+
+        input = "1 4 3";
+        assertFalse(inputValidator.validate(input));
+    }
+
+    @Test
+    public void should_returnFalse_when_inputNumberBiggerThan9(){
+        String input = "13 3 4 5";
+        assertFalse(inputValidator.validate(input));
+    }
 }
--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,29 @@
 package tw.core;
 
 
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
+    private RandomIntGenerator randomIntGenerator;
 
+    public RandomIntGeneratorTest(){
+        randomIntGenerator = mock(RandomIntGenerator.class);
+    }
+
+    @Test
+    public void should_returnNumberString_when_generateNums(){
+        when(randomIntGenerator.generateNums(10, 4)).thenReturn("1 2 3 4");
+        String result = randomIntGenerator.generateNums(10, 4);
+        assertThat(result, is("1 2 3 4"));
+    }
 }
